9763a87234e29c69a793d5791171a092c4702093
Ashish Jat <ashishjat177@gmail.com>
2025-05-13 22:37:59 +0530

    feat/star rating

---
+++ star-rating/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import StartRating from './components/StarRating'
+
+function App() {
+
+  return (
+    <>
+     <StartRating starCount={5}/>
+    </>
+  )
+}
+
+export default App
